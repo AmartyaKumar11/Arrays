dfef4b1ed9f22600796117bc3cdc3d6669c4c49f
Amartya Kumar <157455233+AmartyaKumar11@users.noreply.github.com>
2024-06-04 17:08:15 +0530

    Update Largest_element_in_an_Array.cpp

---
+++ Largest_element_in_an_Array.cpp
@@ -0,0 +1,9 @@
+Brute Force Approach : 
+We can sort the array in ascending order, hence the largest element will be at the last index of the array.
+Time Complexity: O(N*log(N))
+Space Complexity: O(n)
+
+Recursive Approach :
+We can maintain a max variable that will update whenever the current value is greater than the value in the max variable.
+Time Complexity: O(N)
+Space Complexity: O(1)
