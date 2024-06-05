30001e3adbbc8d41f45f52aa14c772771ee74c63
Amartya Kumar <157455233+AmartyaKumar11@users.noreply.github.com>
2024-06-05 09:01:11 +0530

    Update Check_if_an_Array_is_sorted.cpp

--- Check_if_an_Array_is_sorted.cpp
+++ Check_if_an_Array_is_sorted.cpp
@@ -39,7 +39,8 @@ int main() {
 //Optimal Approach :
 /*
 As we know that for a sorted array the previous of every element is smaller than or equal to its current element.
-So, Through this, we can conclude that if the previous element is smaller than or equal to the current element then. Then we can say that the two elements are sorted. If the condition is true for the entire array then the array is sorted.
+So, Through this, we can conclude that if the previous element is smaller than or equal to the current element then. 
+Then we can say that the two elements are sorted. If the condition is true for the entire array then the array is sorted.
 We will check every element with its previous element if the previous element is smaller than or equal to the current element then we will move to the next index.
 If the whole array is traversed successfully or the size of the given array is zero or one (i.e N = 0 or N = 1). Then we will return True else return False.
 */
